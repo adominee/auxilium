af21a05998500ba13a28c624d08a546234db4a2d
adominee <blackcalicocat@gmail.com>
2021-01-08 18:40:38 +0900

    目標の一覧・個別表示の実装 目標の作成と表示のテスト

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 const request=require('supertest');
 const app=require('../app');
 const passportStub=require('passport-stub');
+const User = require('../models/user');
+const Goal = require('../models/goal');
 
 describe('/login',()=>{
   beforeAll(()=>{
@@ -36,3 +38,51 @@ describe('/login',()=>{
       .expect(302);
   })
 })
+
+describe('/goal',()=>{
+  beforeAll(()=>{
+    passportStub.install(app);
+    passportStub.login({id:0,username:'testuser'});
+  });
+  
+  afterAll(()=>{
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+    
+  test('目標の作成ができ、表示される', done => {
+    User.upsert({userId:0,username:'testuser'}).then(()=>{
+      //作成できるかのテスト
+      request(app)
+        .post('/goal')
+        .send({
+          goalName:'テスト目標1',
+          deadline:'2021-01-08T08:52:18.051Z',
+          comment:'テストテストテストテスト'
+        })
+        .expect('Location',/goal/)
+        .expect(302)
+        .end((err,res)=>{
+          const createdGoalPath=res.headers.location;
+          //表示されるかのテスト
+          request(app)
+            .get(createdGoalPath)
+            .expect(/テスト目標1/)
+            .expect(/Fri Jan 08 2021/)
+            .expect(/テストテストテストテスト/)
+            .expect(200)
+            .end((err,res)=>{
+              if(err)return done(err);
+              //テストで作成したデータを削除
+              const goalId=createdGoalPath.split('/goal/')[1];
+              Goal.findByPk(goalId).then(s=>{
+                s.destroy().then(()=>{
+                  if(err) return done(err);
+                  done();
+                });
+              });
+            });
+        });
+    });
+  });
+});
