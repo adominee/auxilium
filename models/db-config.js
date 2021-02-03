b1859233793e0489d6f0feb42e0b7d6c7a303f42
adominee <blackcalicocat@gmail.com>
2021-02-03 21:15:45 +0900

    Heroku対応

--- models/db-config.js
+++ models/db-config.js
@@ -1,15 +1,7 @@
 'use strict';
 const {Sequelize}=require('sequelize');
-const dbConfig=new Sequelize('auxilium','postgres','postgres',{
-  host:'localhost',
-  dialect:'postgres',
-
-  pool:{
-    max:5,
-    min:0,
-    acquire:30000,
-    idle:10000
-  }
-});
+const dbConfig=new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/auxilium'
+);
 
 module.exports=dbConfig
